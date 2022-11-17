44e99bffeb651d9b7a16828d4e9f89671fa541dd
irfanALI <irfan8108@gmail.com>
2022-11-17 19:15:37 +0530

    razor test credentials updated

--- config/razor.php
+++ config/razor.php
@@ -6,6 +6,6 @@ return [
     // 'secret' => 'OY05FFoa2q6OAv9W7gJENF9Q'
     
     // TEST
-    'key' => 'rzp_test_DMBAs0PwXAlwm0',
-    'secret' => '15ZNjbWKkd96qTnniwN5KOvy'
+    'key' => 'rzp_test_qc5VkXdojv08yT',
+    'secret' => 'bUlabozFnYd8PLCAk85lrA32'
 ];
